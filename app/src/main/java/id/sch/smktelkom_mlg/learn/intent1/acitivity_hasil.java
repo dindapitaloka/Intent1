aa60ad66ca2aa38ce8178df4ccbc48b597b2e250
dindapitaloka <dindapitaloka98@gmail.com>
2016-10-30 14:59:42 +0700

    startActivity Part 1

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/acitivity_hasil.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/acitivity_hasil.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.learn.intent1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HasilActivity extends AppCompatActivity {
 
@@ -10,6 +12,11 @@ public class HasilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil);
 
-        findViewById(R.id.buttonHitung)
+        findViewById(R.id.buttonHitung).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HasilActivity.this, HasilActivity.class));
+            }
+        });
     }
 }
